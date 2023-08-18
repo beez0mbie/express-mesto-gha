c7f6481a0168fb96f8ba5ea9787ca0e12f559840
beez0mbie <vzhik3268@gmail.com>
2023-08-18 11:10:53 +0300

    fix joi url pattern

--- app.js
+++ app.js
@@ -36,11 +36,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({
-      scheme: [
-        /^https?:\/\/(www\.)?[a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]{1,256}#?/,
-      ],
-    }),
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]{1,256}#?/),
   }).unknown(true),
 }), createUser);
 app.post('/signin', celebrate({
