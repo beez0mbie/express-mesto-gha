b6623ad64f3546f532469ed69e3188784606ed7e
beez0mbie <vzhik3268@gmail.com>
2023-08-18 11:19:43 +0300

    set default to ENV variables

--- utils/getJwtSecretKey.js
+++ utils/getJwtSecretKey.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET, JWT_DEV } = process.env;
+const { NODE_ENV = 'production', JWT_SECRET = 'production_secret', JWT_DEV = 'dev_secret' } = process.env;
 
 const getJwtSecretKey = () => (NODE_ENV === 'production' ? JWT_SECRET : JWT_DEV);
 
