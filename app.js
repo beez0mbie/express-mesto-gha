7bd91a2ef9bb4f70ee3b95c8aa99b71069d5ff92
beez0mbie <vzhik3268@gmail.com>
2023-08-21 12:40:09 +0300

    remove unknown(true)

--- app.js
+++ app.js
@@ -41,14 +41,14 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(urlRegExp),
-  }).unknown(true),
+  }),
 }), createUser);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
 
-  }).unknown(true),
+  }),
 }), login);
 app.use(auth);
 app.use(router);
